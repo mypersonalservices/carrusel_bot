26ce869ca3318e7c777f4e1153be2b7af57d7453
carrusel_bot <carrusel@carrusel_bot_service.com>
2022-08-30 12:56:03 +0200

    Make sure deploy devel page script ALWAYS make a successful stash push/pop cycle to avoid dangerous corner cases

--- deploy_devel_page.sh
+++ deploy_devel_page.sh
@@ -3,6 +3,7 @@
 set -e
 
 ./utils/render_round.py
+touch TRASH_FILE_TO_FORCE_STASH
 git stash -u
 git checkout gh-pages
 cp -R ./utils/build/* .
@@ -15,3 +16,4 @@ git commit -m "Auto deploy from CLI"
 git push origin gh-pages
 git checkout master
 git stash pop
+rm TRASH_FILE_TO_FORCE_STASH
